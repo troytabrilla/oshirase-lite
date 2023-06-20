622ef48754afd88c83c942a48400e92817d34668
Troy Tabrilla <troy.tabrilla+git@gmail.com>
2023-06-20 08:20:38 -0700

    Refactor auth model.

--- src/app/api/anilist/authorize/route.ts
+++ src/app/api/anilist/authorize/route.ts
@@ -1,17 +1,25 @@
 import { NextRequest, NextResponse } from "next/server"
+import { NextURL } from "next/dist/server/web/next-url"
 import { add } from "date-fns"
 
-import getAccessToken from "./lib/get-access-token"
-import setupRedirect from "./lib/setup-redirect"
+import { AniListAuth } from "./models/anilist-auth"
 import errorHandler from "../../lib/error-handler"
+import { BadRequest } from "../../lib/errors"
 
 export const GET = async (req: NextRequest): Promise<NextResponse> => {
     try {
         const { searchParams } = new URL(req.url)
 
-        const { access_token, expires_in } = await getAccessToken({
-            auth_code: searchParams.get("code"),
-        })
+        const auth = new AniListAuth()
+        await auth.fetch({ auth_code: searchParams.get("code") })
+
+        const data = auth.serialize()
+
+        if (!data) {
+            throw new BadRequest("Could not retrieve access token")
+        }
+
+        const { access_token, expires_in } = data
 
         const { url, sameSite } = setupRedirect(req)
 
@@ -30,4 +38,31 @@ export const GET = async (req: NextRequest): Promise<NextResponse> => {
     } catch (err: any) {
         return errorHandler(err)
     }
+}
+
+type URL = string | NextURL
+type SameSite = boolean | "lax" | "strict" | "none" | undefined
+
+const setupRedirect = (
+    req: NextRequest
+): {
+    url: URL
+    sameSite: SameSite
+} => {
+    let url: URL
+    let sameSite: SameSite
+
+    if (process.env.NODE_ENV === "development") {
+        url = (process.env.ANILIST_OAUTH_REDIRECT_URI as string).replace(
+            "/api/anilist/authorize",
+            "/"
+        )
+        sameSite = "lax"
+    } else {
+        url = req.nextUrl.clone()
+        url.pathname = "/"
+        sameSite = "strict"
+    }
+
+    return { url, sameSite }
 }
